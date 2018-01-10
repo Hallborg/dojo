c31e1fb9067227ded1585d66736799304966f678
jesper.hallis <jesper.hallis@gmail.com>
2018-01-10 07:54:45 -0500

    initial anagram solution

--- katas/anagram/anagram.py
+++ katas/anagram/anagram.py
@@ -1,2 +1,18 @@
 import requests
+anagrams = []
 words = requests.get("http://codekata.com/data/wordlist.txt").text.split()
+for word in words:
+  if len(anagrams) is 0:
+    anagrams.append([word])
+  else:
+    found = False
+    for i, sets in enumerate(anagrams):
+      if ''.join(sorted(word)) == ''.join(sorted(sets[0])):
+        anagrams[i].append(word)
+        found = True
+        break
+
+    if found is False:
+      anagrams.append([word])
+
+print len(anagrams)
